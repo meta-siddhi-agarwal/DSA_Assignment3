46e632402d152a623998e5a90cdcdca1b583e35d
Siddhi Agarwal <siddhi.agarwal@metacube.com>
2025-04-04 22:22:11 +0530

    initial commit

---
+++ src/pac/DirectoryStructure.java
@@ -0,0 +1,16 @@
+package pac;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DirectoryStructure {
+    String name;
+    Queue<DirectoryStructure> childNodes=new LinkedList<>();
+
+    public DirectoryStructure() {
+        
+    }
+
+    
+
+}
